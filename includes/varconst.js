c6323e97b3e59b58e526e290bb17df54d60ab113
Manikandan PM <pcbzmani@outlook.com>
2024-10-22 19:14:52 +0000

    Adding env

--- includes/varconst.js
+++ includes/varconst.js
@@ -2,11 +2,10 @@ const order_id = `A unique identifier for a order`;
 const order_date = `Date of the order`;
 const amount = `Amount spend on that order id`;
 const gs_bucket = `gs://learn_bucket_qr`;
-var today = new Date('2013-09-01');
+var today = new Date('2013-10-01');
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); 
 var yyyy = today.getFullYear();
-
 
 
 module.exports = {
@@ -16,5 +15,7 @@ module.exports = {
    gs_bucket,
    dd,
    mm,
-   yyyy
+   yyyy,
+   tablePrefix: dataform.projectConfig.vars.table_prefix,
+   schema : dataform.projectConfig.vars.env
 };
